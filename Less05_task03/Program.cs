7f35461327c80c988a6b4d5782de2f0883ca09fa
Hayk <hayk.hayr3@gmail.com>
2020-12-09 20:45:59 +0400

    Add project files.

---
+++ Less05_task03/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Less05_task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+
+            Console.WriteLine(instance[2]);
+            //выход за пределы массива
+            Console.WriteLine(instance[10]);
+
+            Console.ReadKey();
+        }
+    }
+
+    class MyClass
+    {
+        string[] array = new string[3] { "one", "two", "three" };
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < array.Length)
+                {
+                    return array[index];
+                }
+                return "index out of range";
+            }
+            set
+            {
+                if (index >= 0 || index < array.Length)
+                {
+                    array[index] = value;
+                }
+            }
+        }
+    }
+}
